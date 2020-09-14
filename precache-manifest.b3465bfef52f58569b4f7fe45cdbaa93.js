d8ce5db9d9e53276711c31dfa21ed2280ae43570
Cho Won Jae <yoteamo7@naver.com>
2020-09-14 19:43:26 +0900

    Updates

---
+++ precache-manifest.b3465bfef52f58569b4f7fe45cdbaa93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "995003aee0ff3de1c0e0fdfc526dcc85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70d758f74fae934d20d0",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "95e0a04ecf585f3a4d38",
+    "url": "/static/css/main.449727e6.chunk.css"
+  },
+  {
+    "revision": "70d758f74fae934d20d0",
+    "url": "/static/js/2.7608c1cf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7608c1cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95e0a04ecf585f3a4d38",
+    "url": "/static/js/main.50066472.chunk.js"
+  },
+  {
+    "revision": "b8fe7cbaefbe45a059dc",
+    "url": "/static/js/runtime-main.721971a8.js"
+  }
+]);
